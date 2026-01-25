85aa804030fdbfe50bc7e36498fdd00a30dca68e
Sam Loeffler <samloeffler123@gmail.com>
2026-01-25 10:00:31 -0600

    main file

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import requests
-import os
-from dotenv import load_dotenv
+from loop_through_playlist import main_loop
+from auth_code_flow import request_access_token, request_user_authorization
+from add_songs_to_playlist import add_songs
+from constants import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, INITIAL_TOKEN, IP_ADDRESS, PORT, MAIN_PLAYLIST_URL, NEW_PLAYLIST_URL, ACCESS_URL, AUTHORIZE_URL
 
-load_dotenv()
+def main():
+    list_of_songs = main_loop(MAIN_PLAYLIST_URL, INITIAL_TOKEN)
+    access_token = request_user_authorization(AUTHORIZE_URL, CLIENT_ID, REDIRECT_URI, PORT, IP_ADDRESS)
+    final_access_token = request_access_token(access_token, REDIRECT_URI, CLIENT_ID, CLIENT_SECRET, ACCESS_URL)
+    add_songs(list_of_songs, final_access_token, NEW_PLAYLIST_URL)
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
-token_url = "https://accounts.spotify.com/api/token"
-
-data = {
-    "grant_type": "client_credentials",
-    "client_id": client_id,
-    "client_secret": client_secret
-}
-
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-
-response = requests.post(token_url, data=data, headers=headers)
-
-token_response = response.json()
-
-print(token_response)
+if __name__ == "__main__":
+    main()
